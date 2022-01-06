8688ca3f12f609f7a7a6445217be28e8620c9610
Missingid <missing000313@gmail.com>
2022-01-06 18:13:02 +0800

    main_version

--- test_20220104.py
+++ test_20220104.py
@@ -2,3 +2,10 @@ print("hello world")
 a=1
 b=2
 print(a+b)
+
+cars=['audi','bmw','subaru','toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
